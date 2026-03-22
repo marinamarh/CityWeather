676480edc8b7ce62ea16655fed134a061f1a7bde
Marina Marhitych <margmarina9@gmail.com>
2026-03-22 20:03:19 +0200

    Initial project setup

---
+++ CityWeather/CityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherApp.swift
+//  CityWeather
+//
+//  Created by Marina Marhitych on 22.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityWeather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CityWeather
+//
+//  Created by Marina Marhitych on 22.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
